8f0f328cb522ea97b1039eef28227c40d27860e7
Richard <richcrwo@gmail.com>
2025-10-18 16:01:29 +0300

    [CHANGE] xephyr script
    Update xephyr script

--- run-dwm-xephyr.sh
+++ run-dwm-xephyr.sh
@@ -2,10 +2,14 @@
 
 killall Xephyr 2>/dev/null
 
-Xephyr -br -ac -noreset -screen 1280x720 :1 &
+Xephyr -br -ac -noreset -screen 1280x720 :4 &
 sleep 1
 
-DISPLAY=:1
-DISPLAY=:1 feh --bg-fill ~/wallpapers/test.jpg &
-DISPLAY=:1 dwmblocks &
-DISPLAY=:1 ./dwm &
+
+DISPLAY=:4
+DISPLAY=:4 feh --bg-fill ~/wallpapers/o3twgi968xuf1.png &
+DISPLAY=:4 wal --backend xrdb -i ~/wallpapers/o3twgi968xuf1.png
+DISPLAY=:4 xrdb -merge ~/.cache/wal/colors.Xresources
+# DISPLAY=:4 feh --bg-fill ~/wallpapers/o3twgi968xuf1.png &
+# DISPLAY=:4 dwmblocks &
+DISPLAY=:4 ./dwm &
